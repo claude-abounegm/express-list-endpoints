3faabceddd08ce09883697578073d53e4319b311
Claude Abounegm <claude.abounegm@gmail.com>
2021-05-28 23:59:25 +0200

    refactoring to class based and adding options
    - option to show parent middleware

---
+++ src/ExpressListEndPoints.js
@@ -0,0 +1,244 @@
+const regexpExpressRegexp =
+  /^\/\^\\\/(?:(:?[\w\\.-]*(?:\\\/:?[\w\\.-]*)*)|(\(\?:\(\[\^\\\/]\+\?\)\)))\\\/.*/;
+const expressRootRegexp = "/^\\/?(?=\\/|$)/i";
+const regexpExpressParam = /\(\?:\(\[\^\\\/]\+\?\)\)/g;
+const STACK_ITEM_VALID_NAMES = ["router", "bound dispatch", "mounted_app"];
+
+class ExpressListEndPoints {
+  options = {
+    showParentMiddleware: false,
+    showAllRoutes: false,
+    showAnonymous: true,
+  };
+
+  /**
+   *
+   * @param {{ showParentMiddleware?: boolean; showAllRoutes?: boolean; showAnonymous?: boolean }} options
+   */
+  constructor(options) {
+    if (typeof options === "object") {
+      this.options = {
+        ...this.options,
+        ...options,
+      };
+    }
+  }
+
+  /**
+   * Returns all the verbs detected for the passed route
+   */
+  _getRouteMethods(route) {
+    const methods = [];
+
+    for (const method in route.methods) {
+      if (method === "_all" && !this.options.showAllRoutes) {
+        continue;
+      }
+
+      methods.push(method.toUpperCase());
+    }
+
+    return methods;
+  }
+
+  _getMiddlewareName(middleware) {
+    let name = middleware.handle.name;
+
+    if (!name && this.options.showAnonymous) {
+      name = "anonymous";
+    }
+
+    return name;
+  }
+
+  /**
+   * Returns the names (or anonymous) of all the middleware attached to the
+   * passed route
+   */
+  _getRouteMiddleware(route) {
+    return route.stack
+      .map((item) => this._getMiddlewareName(item))
+      .filter((v) => !!v);
+  }
+
+  /**
+   * Returns true if found regexp related with express params
+   */
+  _hasParams(pathRegexp) {
+    return regexpExpressParam.test(pathRegexp);
+  }
+
+  /**
+   * @param {Object} route Express route object to be parsed
+   * @param {string} basePath The basePath the route is on
+   * @return {Object[]} Endpoints info
+   */
+  _parseExpressRoute(route, basePath, parentMiddleware = []) {
+    const endpoints = [];
+    const middleware = [
+      ...parentMiddleware,
+      ...this._getRouteMiddleware(route),
+    ];
+
+    if (Array.isArray(route.path)) {
+      route.path.forEach((path) => {
+        endpoints.push({
+          path: basePath + (basePath && path === "/" ? "" : path),
+          methods: this._getRouteMethods(route),
+          middleware,
+        });
+      });
+    } else {
+      endpoints.push({
+        path: basePath + (basePath && route.path === "/" ? "" : route.path),
+        methods: this._getRouteMethods(route),
+        middleware,
+      });
+    }
+
+    return endpoints;
+  }
+
+  _parseExpressPath(expressPathRegexp, params) {
+    let parsedPath = regexpExpressRegexp.exec(expressPathRegexp);
+    let parsedRegexp = expressPathRegexp;
+    let paramIdx = 0;
+
+    while (hasParams(parsedRegexp)) {
+      const paramId = ":" + params[paramIdx].name;
+
+      parsedRegexp = parsedRegexp
+        .toString()
+        .replace(/\(\?:\(\[\^\\\/]\+\?\)\)/, paramId);
+
+      paramIdx++;
+    }
+
+    if (parsedRegexp !== expressPathRegexp) {
+      parsedPath = regexpExpressRegexp.exec(parsedRegexp);
+    }
+
+    parsedPath = parsedPath[1].replace(/\\\//g, "/");
+
+    return parsedPath;
+  }
+
+  /**
+   * Ensures the path of the new endpoints isn't yet in the array.
+   * If the path is already in the array merges the endpoints with the existing
+   * one, if not, it adds them to the array.
+   *
+   * @param {Array} endpoints Array of current endpoints
+   * @param {Object[]} newEndpoints New endpoints to be added to the array
+   * @returns {Array} Updated endpoints array
+   */
+  _addEndpoints(endpoints, newEndpoints) {
+    newEndpoints.forEach((newEndpoint) => {
+      const foundEndpointIdx = endpoints.findIndex(
+        (item) => item.path === newEndpoint.path
+      );
+
+      if (foundEndpointIdx > -1) {
+        const foundEndpoint = endpoints[foundEndpointIdx];
+
+        const newMethods = newEndpoint.methods.filter(
+          (method) => foundEndpoint.methods.indexOf(method) === -1
+        );
+
+        foundEndpoint.methods = foundEndpoint.methods.concat(newMethods);
+      } else {
+        endpoints.push(newEndpoint);
+      }
+    });
+
+    return endpoints;
+  }
+
+  _parseEndpoints({ app, basePath, endpoints, parentMiddleware = [] }) {
+    const stack = app.stack || (app._router && app._router.stack);
+
+    endpoints = endpoints || [];
+    basePath = basePath || "";
+
+    const middleware = [...parentMiddleware];
+
+    if (!stack) {
+      this._addEndpoints(endpoints, [
+        {
+          path: basePath,
+          methods: [],
+          middlewares: [],
+        },
+      ]);
+    } else {
+      stack.forEach((stackItem) => {
+        if (stackItem.route) {
+          const newEndpoints = this._parseExpressRoute(
+            stackItem.route,
+            basePath,
+            middleware
+          );
+
+          endpoints = this._addEndpoints(endpoints, newEndpoints);
+        } else if (STACK_ITEM_VALID_NAMES.indexOf(stackItem.name) > -1) {
+          if (regexpExpressRegexp.test(stackItem.regexp)) {
+            const parsedPath = this._parseExpressPath(
+              stackItem.regexp,
+              stackItem.keys
+            );
+
+            this._parseEndpoints({
+              app: stackItem.handle,
+              basePath: `${basePath}/${parsedPath}`,
+              endpoints,
+              parentMiddleware: middleware,
+              options,
+            });
+          } else if (
+            !stackItem.path &&
+            stackItem.regexp &&
+            stackItem.regexp.toString() !== expressRootRegexp
+          ) {
+            const regEcpPath = " RegExp(" + stackItem.regexp + ") ";
+
+            this._parseEndpoints({
+              app: stackItem.handle,
+              basePath: `${basePath}/${regEcpPath}`,
+              endpoints,
+              parentMiddleware: middleware,
+              options,
+            });
+          } else {
+            this._parseEndpoints({
+              app: stackItem.handle,
+              basePath,
+              endpoints,
+              parentMiddleware: middleware,
+              options,
+            });
+          }
+        } else if (this.options.showParentMiddleware) {
+          const name = this._getMiddlewareName(stackItem);
+
+          if (name) {
+            middleware.push(name);
+          }
+        }
+      });
+    }
+
+    return endpoints;
+  }
+
+  /**
+   * Returns an array of strings with all the detected endpoints
+   * @param {Object} app the express/route instance to get the endpoints from
+   */
+  getEndpoints(app) {
+    const endpoints = this._parseEndpoints({ app });
+
+    return endpoints;
+  }
+}
+
+module.exports = ExpressListEndPoints;
